b04b6844eaa441c425e448c92f152e0e8b175973
Hokir <chenmax02@gmail.com>
2022-02-03 13:13:44 +0100

    README.md

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#!/bin/bash
 #include <stdio.h>
-
-int main(void)
-
+/*
+ * main is the first function of any C scripts
+ */int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-
-  return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+ /* description main function */
 }
