0dd4e2b536064bc795e0ed2bd9734384f9130b0b
Hokir <chenmax02@gmail.com>
2022-02-10 04:04:38 +0100

    Prints number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,8 +4,8 @@
  * void prints the numbers, from 0 to 9
  */ void print_numbers(void)
  {
-int num;
+char num;
 for (num = 0; num <= 9; num++)
-  _putchar((num % 10) + '0');
+_putchar((num % 10) + '0');
 _putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,10 +1,20 @@
 #include "main.h"
 
-void print_numbers(void)
+/*
+ * prints most numbers
+ */ void print_most_numbers(void)
 {
-int num;
-for (num = 0; num <= 9; num++)
-if ((num != 2) || (num != 4))
-_putchar((num % 10) + '0');
+int num = 0;
+for (; num <= 9; num++)
+{
+if (num == 2 || num == 4)
+{
+continue;
+}
+else
+{
+_putchar(num + '0');
+}
+}
 _putchar('\n');
 }
