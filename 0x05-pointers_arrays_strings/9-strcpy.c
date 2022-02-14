b7f428d3bccde02fc21d4b775a825326a0660e2f
Hokir <chenmax02@gmail.com>
2022-02-14 23:54:13 +0100

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ *
+ */ char *_strcpy(char *dest, char *src)
+{
+int i;
+
+i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+dest[i] = '\0';
+return (dest);
+}
